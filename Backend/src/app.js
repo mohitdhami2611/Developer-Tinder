81f0fe159b191ac4fd35d8f5641e01bf3781be5c
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-12-12 20:28:52 +0530

    created express server and added script to package.json

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,15 @@
-import React from 'react'
+const express = require("express");
+const app = express();
 
-const app = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+app.get("/line", (req, res) => {
+  res.send("hello how are you");
+});
 
-export default app
+// Default route for all other paths
+app.use((req, res) => {
+  res.send("hello the port is active and well maintained by me");
+});
+
+app.listen(3000, () => {
+  console.log("the server on port 3000 is started");
+});
