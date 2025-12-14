1e256f52ef0310d5cac6a19452753b6a3725591f
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-12-14 19:10:57 +0530

    ,

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,5 +11,5 @@ app.use((req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("the server on port 3000 is started and all good to run ");
+  console.log("the server on port 3000 is started and all good to run for it ");
 });
