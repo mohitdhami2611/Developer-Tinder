c081bb6488e4dd76a10ebd166b76a6c7d74354fd
MOHIT DHAMI <148179834+mohitdhami2611@users.noreply.github.com>
2025-12-24 21:17:23 +0530

    just made a minor changes to it

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,7 @@ connectDb()
   .then(() => {
     console.log("database connected succesfully");
     app.listen(7777,() => {
-        console.log("server is successfully running and  listening on port number 7777")
+        console.log("server is successfully running and  listening on port number 7777 and is doing great and fine")
     })
   })
   .catch((err) => {
